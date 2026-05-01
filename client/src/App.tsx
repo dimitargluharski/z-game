1c9827124c85c8eb69d43fcf2cf0dda069aebe4b
Dimitar Gluharski <dimitargluharski@gmail.com>
2026-05-01 19:32:10 +0300

    initialize server and shared packages with TypeScript setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { test } from '@shared/test';
+console.log(test);
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ client/vite.config.ts
@@ -0,0 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+// })
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared')
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/colyseus': {
+        target: 'ws://localhost:2567',
+        ws: true
+      }
+    }
+  }
+})
